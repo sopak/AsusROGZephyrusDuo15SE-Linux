e1d2f1a1dbd9c0fe725b7266022ba589c8d4de14
Kamil Sopko <kamil.sopko@jcode.cz>
2021-02-13 16:20:10 +0100

    Cleanup

--- touchscreen.sh
+++ touchscreen.sh
@@ -1,9 +1,30 @@
+#!/bin/bash
+
 # setup independent cursor input for touchscreen
-xinput create-master TouchScreenGX551QS
-TPID=$(xinput --list --id-only "TouchScreenGX551QS pointer")
-TTID=$(xinput --list |grep ELAN|grep -v keyboard)
-TTID=${TTID#*id=}
-TTID=${TTID%*[*}
-xinput enable ${TPID}
-xinput reattach ${TTID} ${TPID}
-xinput map-to-output ${TTID} DisplayPort-0
+
+MASTER_NAME=TouchScreenGX551QS
+SLAVE_PREFIX=ELAN
+MONITOR_ID=DisplayPort-0
+
+if ! xinput | grep ${MASTER_NAME} >/dev/null ; then
+    xinput create-master ${MASTER_NAME}
+fi
+
+MASTER_ID=$(xinput --list --id-only "${MASTER_NAME} pointer")
+
+SLAVE_ID=$(xinput --list |grep ${SLAVE_PREFIX}|grep -v keyboard)
+SLAVE_ID=${SLAVE_ID#*id=}
+SLAVE_ID=${SLAVE_ID%*[*}
+
+
+echo "MASTER NAME: ${MASTER_NAME}"
+echo "MASTER ID: ${SLAVE_ID}"
+echo "SLAVE NAME: $(xinput list --name-only |grep ${SLAVE_PREFIX}|head -n 1)"
+echo "SLAVE ID: ${MASTER_ID}"
+
+xinput enable ${MASTER_ID}
+xinput enable ${SLAVE_ID}
+xinput reattach ${SLAVE_ID} ${MASTER_ID}
+xinput map-to-output ${SLAVE_ID} ${MONITOR_ID}
+
+echo "Done"
